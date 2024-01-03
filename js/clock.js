e5eae5e9d4e00d9c805d32d1886e3039dbac9196
İbrahim Sancaklı <ibrahimsancakli@gmail.com>
2024-01-04 01:37:07 +0300

    change

---
+++ js/clock.js
@@ -0,0 +1,16 @@
+let user = prompt("Kullanıcı adınızı giriniz: ");
+let userName = document.querySelector("#myName");
+userName.innerHTML = user;
+
+setInterval(showTime, 1000);
+
+function showTime() {
+    let date = new Date();
+    const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
+    let hour = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+    let minute = date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+    let second = date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+    let day = days[date.getDay()];
+    document.querySelector("#myClock").innerHTML = `${hour}:${minute}:${second} ${day}`;
+
+}
