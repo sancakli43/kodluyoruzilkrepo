d24a60676804238aacd91ea7b9614ab085b16018
İbrahim Sancaklı <ibrahimsancakli@gmail.com>
2024-01-22 05:15:07 +0300

    odev2

--- odev2/todolist.js
+++ odev2/todolist.js
@@ -1,6 +1,36 @@
-let list = document.querySelectorAll("li");
-list.forEach(item => {
-    item.addEventListener("click", function(){
-        (this.classList.value != "checked" ? this.classList.add("checked") : this.classList.remove("checked"))
-    })
+let ul = document.querySelector("ul");
+ul.addEventListener("click", ev => {
+    if (ev.target.tagName === "LI") {
+        ev.target.classList.toggle("checked");
+    }
+    else if (ev.target.tagName === "SPAN") {
+        ev.target.parentElement.style.display = "none";
+    }
 })
+
+let li = document.querySelectorAll("li");
+li.forEach(item => {
+    createSpan(item);
+})
+
+function createSpan(item) {
+    let span = document.createElement("span");
+    let x = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(x);
+    item.appendChild(span);
+}
+
+function newElement() {
+    let li = document.createElement("li");
+    let task = document.querySelector("#task").value;
+    if (task == "" || task.trim().length == 0) {
+        $(".error").toast("show");
+    } else {
+        $(".success").toast("show");
+        li.innerHTML = task;
+        createSpan(li);
+        ul.appendChild(li);
+        document.querySelector("#task").value = "";
+    }
+}
